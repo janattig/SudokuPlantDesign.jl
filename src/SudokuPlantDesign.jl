cf490749a6641f25bdf71f879ddc09cdd50f4d77
Jan Attig <jattig@thp.uni-koeln.de>
2022-03-27 17:29:37 +0200

    added includes for packages

--- src/SudokuPlantDesign.jl
+++ src/SudokuPlantDesign.jl
@@ -1,12 +1,19 @@
 module SudokuPlantDesign
 
-    # include the sub files
+    # module loading
+    using BlockArrays
+    using ProgressMeter
+    using PyPlot
+    using Statistics
+    using LinearAlgebra
 
-    # check configuration
+
+
+    # include files for check configuration
     include("check_configuration/type_definition.jl")
     include("check_configuration/interface.jl")
     include("check_configuration/utility_functions.jl")
 
-    # monte carlo
+    # include files for monte carlo
 
 end
