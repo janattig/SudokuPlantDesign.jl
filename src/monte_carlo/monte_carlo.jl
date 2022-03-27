11067e3b32d961d28ba0e808ab885793e85a436f
Jan Attig <jattig@thp.uni-koeln.de>
2022-03-27 18:44:03 +0200

    more work on mc part

--- src/monte_carlo/monte_carlo.jl
+++ src/monte_carlo/monte_carlo.jl
@@ -1,7 +1,7 @@
 # a single monte carlo update
-function mc_update!(C, T, updatemoves, K)
+function mc_update!(C, T, updates, K)
     K_old = K(C)
-    updt  = rand(updatemoves)
+    updt  = rand(updates)
     generate!(updt, C)
     apply_update!(C, updt)
     K_new = K(C)
@@ -15,3 +15,18 @@ function mc_update!(C, T, updatemoves, K)
     end
     return nothing
 end
+export mc_update!
+
+
+
+function optimize_design!(C, updates, K, num_updates, beta)
+    cost_values = zeros(num_updates)
+    @showprogress "Optimizing Design ... " for i in 1:length(cost_values)
+        mc_update!(C, beta(i/length(cost_values)), updates, K)
+        cost_values[i] = K(C)
+    end
+    cost_values .-= minimum(cost_values)
+    cost_values .+= 1
+    return cost_values
+end
+export optimize_design!
