0e791fb8a09669d58318dc8b4867dc0a89f2e004
Jan Attig <jattig@thp.uni-koeln.de>
2022-04-30 21:56:35 +0200

    added interface implementation for periodic boundaries

--- src/check_configuration/interface.jl
+++ src/check_configuration/interface.jl
@@ -165,6 +165,61 @@ function set_empty!(conf :: C, i::Int64, j::Int64) where {BA<:BlockArray, C <: C
     # set in the configuration
     conf.configuration[i,j] = -1
 end
+function set_empty!(conf :: C, i::Int64, j::Int64) where {BA<:BlockArray, C <: CheckConfiguration{PBC,BA}}
+    # check which type of field was there before
+    if is_empty(conf, i,j)
+        # do nothing
+    elseif is_genotype(conf, i,j)
+        # find out block indizes
+        bi = get_block_index_x(conf, i)
+        bj = get_block_index_y(conf, j)
+        # remove one plot from statistics
+        conf.num_plots_block[bi,bj] -= 1
+        conf.num_plots_row[j] -= 1
+        conf.num_plots_col[i] -= 1
+        conf.num_plots_total -= 1
+    else
+        # block is check
+        # find out block indizes
+        bi = get_block_index_x(conf, i)
+        bj = get_block_index_y(conf, j)
+        # remove one plot from statistics
+        conf.num_plots_block[bi,bj] -= 1
+        conf.num_plots_row[j] -= 1
+        conf.num_plots_col[i] -= 1
+        conf.num_plots_total -= 1
+        # remove one check from statistics
+        conf.num_checks_block_total[bi,bj] -= 1
+        conf.num_checks_block[bi,bj][get_check(conf, i,j)] -= 1
+        conf.num_checks_row_total[j] -= 1
+        conf.num_checks_row[j][get_check(conf, i,j)] -= 1
+        conf.num_checks_col_total[i] -= 1
+        conf.num_checks_col[i][get_check(conf, i,j)] -= 1
+        conf.num_checks_total -= 1
+        conf.num_checks[get_check(conf,i,j)] -= 1
+        # check neighbors
+        c1 = get_check(conf, i,j)
+        #for x in max(i-conf.dmax,1):min(i+conf.dmax,sizex(conf))
+        #for y in max((j-conf.dmax)+(abs(i-x)),1):min((j+conf.dmax)-(abs(i-x)),sizey(conf))
+        for dx in -conf.dmax:conf.dmax
+        for dy in -conf.dmax + abs(dx):conf.dmax - abs(dx)
+            d = abs(dx) + abs(dy)
+            x = i + dx
+            y = j + dy
+            x = bring_to_range_one_to_a(x, sizex(conf))
+            y = bring_to_range_one_to_a(y, sizey(conf))
+            if is_check(conf, x,y)
+                if !(x==i && y==j)
+                    c2 = get_check(conf,x,y)
+                    conf.neighbor_pairs[min(c1,c2),max(c1,c2)][d] -= 1
+                end
+            end
+        end
+        end
+    end
+    # set in the configuration
+    conf.configuration[i,j] = -1
+end
 
 
 function set_genotype!(conf :: C, i::Int64, j::Int64) where {BC, BA<:BlockArray, C <: CheckConfiguration{BC,BA}}
@@ -205,6 +260,56 @@ function set_genotype!(conf :: C, i::Int64, j::Int64) where {BA<:BlockArray, C <
             if is_check(conf, x,y)
                 d = abs(x-i)+abs(y-j)
                 if d!=0
+                    c2 = get_check(conf,x,y)
+                    conf.neighbor_pairs[min(c1,c2),max(c1,c2)][d] -= 1
+                end
+            end
+        end
+        end
+    end
+    # set in the configuration
+    conf.configuration[i,j] = 0
+end
+function set_genotype!(conf :: C, i::Int64, j::Int64) where {BA<:BlockArray, C <: CheckConfiguration{PBC,BA}}
+    # check which type of field was there before
+    if is_empty(conf, i,j)
+        # find out block indizes
+        bi = get_block_index_x(conf, i)
+        bj = get_block_index_y(conf, j)
+        # add one plot to statistics
+        conf.num_plots_block[bi,bj] += 1
+        conf.num_plots_row[j] += 1
+        conf.num_plots_col[i] += 1
+        conf.num_plots_total += 1
+    elseif is_genotype(conf, i,j)
+        # do nothing
+    else
+        # block is check
+        # find out block indizes
+        bi = get_block_index_x(conf, i)
+        bj = get_block_index_y(conf, j)
+        # remove one check from statistics
+        conf.num_checks_block_total[bi,bj] -= 1
+        conf.num_checks_block[bi,bj][get_check(conf, i,j)] -= 1
+        conf.num_checks_row_total[j] -= 1
+        conf.num_checks_row[j][get_check(conf, i,j)] -= 1
+        conf.num_checks_col_total[i] -= 1
+        conf.num_checks_col[i][get_check(conf, i,j)] -= 1
+        conf.num_checks_total -= 1
+        conf.num_checks[get_check(conf,i,j)] -= 1
+        # check neighbors
+        c1 = get_check(conf, i,j)
+        #for x in max(i-conf.dmax,1):min(i+conf.dmax,sizex(conf))
+        #for y in max((j-conf.dmax)+(abs(i-x)),1):min((j+conf.dmax)-(abs(i-x)),sizey(conf))
+        for dx in -conf.dmax:conf.dmax
+        for dy in -conf.dmax + abs(dx):conf.dmax - abs(dx)
+            d = abs(dx) + abs(dy)
+            x = i + dx
+            y = j + dy
+            x = bring_to_range_one_to_a(x, sizex(conf))
+            y = bring_to_range_one_to_a(y, sizey(conf))
+            if is_check(conf, x,y)
+                if !(x==i && y==j)
                     c2 = get_check(conf,x,y)
                     conf.neighbor_pairs[min(c1,c2),max(c1,c2)][d] -= 1
                 end
@@ -312,6 +417,121 @@ function set_check!(conf :: C, i::Int64, j::Int64, c::Int64) where {BA<:BlockArr
     # set in the configuration
     conf.configuration[i,j] = c
 end
+function set_check!(conf :: C, i::Int64, j::Int64, c::Int64) where {BA<:BlockArray, C <: CheckConfiguration{PBC,BA}}
+    # check which type of field was there before
+    if is_empty(conf, i,j)
+        # find out block indizes
+        bi = get_block_index_x(conf, i)
+        bj = get_block_index_y(conf, j)
+        # add one plot to statistics
+        conf.num_plots_block[bi,bj] += 1
+        conf.num_plots_row[i] += 1
+        conf.num_plots_col[j] += 1
+        conf.num_plots_total += 1
+        # add one check to statistics
+        conf.num_checks_block_total[bi,bj] += 1
+        conf.num_checks_block[bi,bj][c] += 1
+        conf.num_checks_row_total[j] += 1
+        conf.num_checks_row[j][c] += 1
+        conf.num_checks_col_total[i] += 1
+        conf.num_checks_col[i][c] += 1
+        conf.num_checks_total += 1
+        conf.num_checks[c] += 1
+        # check neighbors
+        #for x in max(i-conf.dmax,1):min(i+conf.dmax,sizex(conf))
+        #for y in max((j-conf.dmax)+(abs(i-x)),1):min((j+conf.dmax)-(abs(i-x)),sizey(conf))
+        for dx in -conf.dmax:conf.dmax
+        for dy in -conf.dmax + abs(dx):conf.dmax - abs(dx)
+            d = abs(dx) + abs(dy)
+            x = i + dx
+            y = j + dy
+            x = bring_to_range_one_to_a(x, sizex(conf))
+            y = bring_to_range_one_to_a(y, sizey(conf))
+            if is_check(conf, x,y)
+                if !(x==i && y==j)
+                    c2 = get_check(conf,x,y)
+                    conf.neighbor_pairs[min(c,c2),max(c,c2)][d] += 1
+                end
+            end
+        end
+        end
+    elseif is_genotype(conf, i,j)
+        # find out block indizes
+        bi = get_block_index_x(conf, i)
+        bj = get_block_index_y(conf, j)
+        # add one check to statistics
+        conf.num_checks_block_total[bi,bj] += 1
+        conf.num_checks_block[bi,bj][c] += 1
+        conf.num_checks_row_total[j] += 1
+        conf.num_checks_row[j][c] += 1
+        conf.num_checks_col_total[i] += 1
+        conf.num_checks_col[i][c] += 1
+        conf.num_checks_total += 1
+        conf.num_checks[c] += 1
+        # check neighbors
+        #for x in max(i-conf.dmax,1):min(i+conf.dmax,sizex(conf))
+        #for y in max((j-conf.dmax)+(abs(i-x)),1):min((j+conf.dmax)-(abs(i-x)),sizey(conf))
+        for dx in -conf.dmax:conf.dmax
+        for dy in -conf.dmax + abs(dx):conf.dmax - abs(dx)
+            d = abs(dx) + abs(dy)
+            x = i + dx
+            y = j + dy
+            x = bring_to_range_one_to_a(x, sizex(conf))
+            y = bring_to_range_one_to_a(y, sizey(conf))
+            if is_check(conf, x,y)
+                if !(x==i && y==j)
+                    #println(i, ",", j, " --> ", x,",",y," (d=$(d), dx=$(dx), dy=$(dy))")
+                    c2 = get_check(conf,x,y)
+                    conf.neighbor_pairs[min(c,c2),max(c,c2)][d] += 1
+                end
+            end
+        end
+        end
+    else
+        # block is check
+        # find out block indizes
+        bi = get_block_index_x(conf, i)
+        bj = get_block_index_y(conf, j)
+        # remove one check from statistics and add new one
+        conf.num_checks_block[bi,bj][c] += 1
+        conf.num_checks_block[bi,bj][get_check(conf, i,j)] -= 1
+        conf.num_checks_row[j][c] += 1
+        conf.num_checks_row[j][get_check(conf, i,j)] -= 1
+        conf.num_checks_col[i][c] += 1
+        conf.num_checks_col[i][get_check(conf, i,j)] -= 1
+        conf.num_checks[c] += 1
+        conf.num_checks[get_check(conf, i,j)] -= 1
+        # maybe check neighbors
+        if c != get_check(conf, i,j)
+            c1 = get_check(conf, i,j)
+            #for x in max(i-conf.dmax,1):min(i+conf.dmax,sizex(conf))
+            #for y in max((j-conf.dmax)+(abs(i-x)),1):min((j+conf.dmax)-(abs(i-x)),sizey(conf))
+            for dx in -conf.dmax:conf.dmax
+            for dy in -conf.dmax + abs(dx):conf.dmax - abs(dx)
+                d = abs(dx) + abs(dy)
+                x = i + dx
+                y = j + dy
+                x = bring_to_range_one_to_a(x, sizex(conf))
+                y = bring_to_range_one_to_a(y, sizey(conf))
+                if is_check(conf, x,y)
+                    if !(x==i && y==j)
+                        c2 = get_check(conf,x,y)
+                        conf.neighbor_pairs[min(c,c2),max(c,c2)][d]   += 1
+                        conf.neighbor_pairs[min(c1,c2),max(c1,c2)][d] -= 1
+                    end
+                end
+            end
+            end
+        end
+    end
+    # set in the configuration
+    conf.configuration[i,j] = c
+end
+
+
+
+
+
 
 
 
