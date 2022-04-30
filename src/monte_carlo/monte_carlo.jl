945b1c9ece1ada82b0b8afcc7318d326a5be83e6
Jan Attig <jattig@thp.uni-koeln.de>
2022-04-30 22:31:45 +0200

    added default options for monte carlo algorithm

--- src/monte_carlo/monte_carlo.jl
+++ src/monte_carlo/monte_carlo.jl
@@ -19,7 +19,7 @@ export mc_update!
 
 
 
-function optimize_design!(C, updates, K, num_updates, beta)
+function optimize_design!(C, updates, K, num_updates = 100000, beta = t -> 1*exp(-7*t) * (1 + 0.2*cos(t*30*pi)))
     cost_values = zeros(num_updates)
     @showprogress "Optimizing Design ... " for i in 1:length(cost_values)
         mc_update!(C, beta(i/length(cost_values)), updates, K)
