f79c73c96c36e1408fcb50e6844ac8b9662ca4b4
Jan Attig <jattig@thp.uni-koeln.de>
2022-08-30 19:06:52 +0200

    added function to obtain dataframe from labeled check configuration

--- src/labeled_check_configuration/determine_labels.jl
+++ src/labeled_check_configuration/determine_labels.jl
@@ -35,3 +35,71 @@ function fill_labels!(
 
 end
 export fill_labels!
+
+
+
+function get_dataframe(
+        lconf :: LC
+    ) where {C <: CheckConfiguration, LC <: LabeledCheckConfiguration{C}}
+
+    # create a new dataframe
+    df = DataFrame(
+        index=Int[],
+        x=Int[],y=Int[],
+        block_x=Int[], block_y=Int[],
+        block=String[],
+        check=Int[]
+    )
+
+    # write first batch of information
+    num_props = 0
+    for i in 1:sizex(lconf)
+        for j in 1:sizey(lconf)
+            if lconf.indices[i,j] > 0
+                bx = SudokuPlantDesign.get_block_index_x(lconf.configuration, i)
+                by = SudokuPlantDesign.get_block_index_y(lconf.configuration, j)
+                block = string(bx)*"|"*string(by)
+                check = get_check(lconf.configuration, i,j)
+                num_props = length(lconf.labels[i,j])
+                push!(df, [lconf.indices[i,j],i,j,bx,by,block,check])
+            end
+        end
+    end
+
+    # sort the dataframe
+    sort!(df)
+
+
+    # make new dataframe only for additional information
+    names = [:index, :genotype]
+    cols  = [deepcopy(df[!,:index]), ["NA" for i in 1:size(df)[1]]]
+    for p in 1:num_props-1
+        push!(names, Symbol("property_"*string(p)))
+        push!(cols, ["NA" for i in 1:size(df)[1]])
+    end
+    df_add = DataFrame(cols, names)
+
+    # write additional properties
+    for i in 1:sizex(lconf)
+        for j in 1:sizey(lconf)
+            if lconf.indices[i,j] > 0
+                for li in 1:length(lconf.labels[i,j])
+                    df_add[df_add.index .== lconf.indices[i,j],li+1] .= lconf.labels[i,j][li]
+                end
+            end
+        end
+    end
+
+    # sort the dataframe
+    sort!(df_add)
+
+    # join the two dataframes by index
+    df = innerjoin(df, df_add, on=:index)
+
+    # sort the dataframe
+    sort!(df)
+
+    # return the dataframe
+    return df
+end
+export get_dataframe
